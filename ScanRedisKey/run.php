a86efa0b481ae26ca90c462d9c51280b633c00e6
nwqian <nengwuqian@gmail.com>
2018-07-23 17:02:45 +0800

    add helpers.php function

--- ScanRedisKey/run.php
+++ ScanRedisKey/run.php
@@ -13,38 +13,3 @@ $G_CONF_PATH = array(
 
 $scanKeys = new ScanKeys();
 $scanKeys->scan();
-
-
-//$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-//    $r->addRoute('GET', '/users', 'get_all_users_handler');
-//    // {id} 必须是一个数字 (\d+)
-//    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
-//    //  /{title} 后缀是可选的
-//    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
-//});
-//
-//// 获取请求的方法和 URI
-//$httpMethod = $_SERVER['REQUEST_METHOD'];
-//$uri = $_SERVER['REQUEST_URI'];
-//
-//// 去除查询字符串( ? 后面的内容) 和 解码 URI
-//if (false !== $pos = strpos($uri, '?')) {
-//    $uri = substr($uri, 0, $pos);
-//}
-//$uri = rawurldecode($uri);
-//
-//$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
-//switch ($routeInfo[0]) {
-//    case FastRoute\Dispatcher::NOT_FOUND:
-//        // ... 404 Not Found 没找到对应的方法
-//        break;
-//    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-//        $allowedMethods = $routeInfo[1];
-//        // ... 405 Method Not Allowed  方法不允许
-//        break;
-//    case FastRoute\Dispatcher::FOUND: // 找到对应的方法
-//        $handler = $routeInfo[1]; // 获得处理函数
-//        $vars = $routeInfo[2]; // 获取请求参数
-//        // ... call $handler with $vars // 调用处理函数
-//        break;
-//}
--- ScanRedisKey/src/helpers.php
+++ ScanRedisKey/src/helpers.php
@@ -46,3 +46,57 @@ if (!function_exists('config')) {
         }
     }
 }
+
+/**
+ * @return bool
+ */
+function is_debug_enabled()
+{
+    return true;
+}
+
+/**
+ * @return string
+ */
+function debug_log_path()
+{
+    $logPath = APP_PATH . 'logs/debug';
+    if (is_dir($logPath)) {
+        mkdir($logPath, 0777, true);
+    }
+    return APP_PATH . 'logs/debug/debug-' . date('Ymd') . '.log';
+}
+
+/**
+ * @param string $message //调试信息
+ * @param mixed $context // 调试内容体
+ * @return void
+ */
+function my_debug($message, $context)
+{
+    if (!(is_debug_enabled()))
+    {
+        return;
+    }
+
+    $output = [];
+    $output[] = str_repeat('*', 100);
+
+    // 获取当前进程ID，可以唯一标示一个进程内产生的日志，便于日志的查看和对应
+    $output[] = date('* Y-m-d H:i:s') . ', PHP process ID:' . posix_getpid();
+    $output[] = str_repeat('*', 100);
+    $output[] = $message . PHP_EOL;
+    if (is_array($context))
+    {
+        $output[] = var_export($context, true);
+    }
+    else
+    {
+        $output[] = $context;
+    }
+    $output[] = str_repeat('-', 100);
+    $output[] = str_repeat('-', 100);
+    $output[] = PHP_EOL;
+
+    file_put_contents(debug_log_path(), implode(PHP_EOL, $output), FILE_APPEND);
+}
